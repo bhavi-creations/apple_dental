d0e604762c162054e708cb21fee76621fbaa6fff
manimalladi <manimalladi05@gmail.com>
2026-01-03 14:44:46 +0530

    to change teh name favicon place

--- view_count.php
+++ view_count.php
@@ -34,7 +34,7 @@ $totalCount = $totalRes->fetch_assoc()['total'] ?? 0;
     <!-- 👁 -->
     <img src=".\images1\eye.png" class="img-fluid" alt="" style="width: 30px; height: 30px;">
     <div class="visitor-tooltip">
-        <div>Total Website Visitors: <b><?php echo $totalCount; ?></b></div>
+        <div>Total pages Visitors: <b><?php echo $totalCount; ?></b></div>
         <div>This Page Visitors: <b><?php echo $pageCount; ?></b></div>
     </div>
 </a>
